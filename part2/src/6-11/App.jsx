ff5224cf53ec13edf050d4c256291c32666100a3
Miguel Lozano <miggg89@gmail.com>
2025-07-07 01:12:06 +0200

    exercise 2.11

--- part2/src/6-11/App.jsx
+++ part2/src/6-11/App.jsx
@@ -1,18 +1,22 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import Filter from "./components/Filter.jsx";
 import Form from "./components/Form.jsx";
 import Numbers from "./components/Numbers.jsx";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', number: '040-123456'},
-    {name: 'Ada Lovelace', number: '39-44-5323523'},
-    {name: 'Dan Abramov', number: '12-43-234345'},
-    {name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    axios
+        .get('http://localhost:3001/persons')
+        .then(res => {
+          setPersons(res.data);
+        })
+  })
 
   const addNewPerson = (event) => {
     event.preventDefault()
--- part2/src/main.jsx
+++ part2/src/main.jsx
@@ -1,4 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import App from './6-10/App.jsx'
+import App from './6-11/App.jsx'
 
 createRoot(document.getElementById('root')).render(<App />)
