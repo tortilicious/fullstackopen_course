0901d3578abf7c81972cb4f725f24790bba4074a
Miguel Lozano <miggg89@gmail.com>
2025-07-06 13:02:30 +0200

    update

--- part1/src/6-11/App.jsx
+++ part1/src/6-11/App.jsx
@@ -1,7 +1,8 @@
 import {useState} from 'react'
 
+// unicafe exercise
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
