f2e26340814842b25ed15c47f7a0f1ec9b2fe469
Miguel Lozano <miggg89@gmail.com>
2025-07-10 21:37:16 +0200

    exercises 4.1 / 4.2

---
+++ part4/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogsRouter = require('./controller/blogs')
+
+
+//  ignore non existent fields in our schema
+mongoose.set('strictQuery', false)
+mongoose.connect(config.MONGO_DB_URI)
+    .then(() => logger.info('connected to MongoDB'))
+    .catch(err => logger.error('error connecting to MongoDB', err.message))
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ part4/controller/blogs.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const blogsRouter = express.Router()
+const Blog = require('../model/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+      .find({})
+      .then(blogs => {
+        response.json(blogs)
+      })
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+  blog
+      .save()
+      .then(result => {
+        response.status(201).json(result)
+      })
+      .catch(error => next(error))
+})
+
+module.exports = blogsRouter
