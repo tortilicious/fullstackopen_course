4c748e6d25e5071e02e1be0c43f68e38ba115b6a
Miguel Lozano <miggg89@gmail.com>
2025-07-11 21:27:43 +0200

    exercise 4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -5,6 +5,7 @@ const { test, beforeEach, after, describe} = require('node:test')
 const assert = require('node:assert')
 const { blogsInDB, initializeDB, nonExistingId, initialBlogs } = require('./blog_api_helper')
 const app = require('../app')
+const {runInContext: list} = require("lodash");
 const api = supertest(app)
 
 
@@ -33,6 +34,48 @@ describe('GET /api/blogs', () => {
     blogs.forEach(blog => {
       assert(blog.id, 'Blog should have "id" property')
     })
+  })
+})
+
+describe('POST /api/blogs', () => {
+  test('increases collection length by 1', async() => {
+    const newBlog = {
+      title: 'test title',
+      author: 'test author',
+      url: 'testurl',
+      likes: 10
+    }
+
+    const initialBlogs = await blogsInDB()
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+
+    const finalBlogs = await blogsInDB()
+    assert.strictEqual(finalBlogs.length, initialBlogs.length + 1)
+  })
+  test('checks last POST saves the Blog', async() => {
+    const newBlog = {
+      title: 'test title',
+      author: 'test author',
+      url: 'testurl',
+      likes: 10
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+
+    const blogs = await blogsInDB()
+    const lastBlog = blogs[blogs.length - 1]
+
+    assert.strictEqual(lastBlog.title, newBlog.title)
+    assert.strictEqual(lastBlog.author, newBlog.author)
+    assert.strictEqual(lastBlog.url, newBlog.url)
+    assert.strictEqual(lastBlog.likes, newBlog.likes)
   })
 })
 
